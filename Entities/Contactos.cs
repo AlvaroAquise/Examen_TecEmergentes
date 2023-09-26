c4f41dd2ada49cdfdaf9fcdfeda0031a6933bea6
Portudaddy122 <106115328+Portudaddy122@users.noreply.github.com>
2023-09-26 16:28:23 -0400

    Add files via upload

---
+++ Entities/Contactos.cs
@@ -0,0 +1,12 @@
+
+namespace Entities
+{
+    public class Contactos
+    {
+        public int ID {get; set;}
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+    }
+}
